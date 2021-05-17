b0273cb64852f91e5abd8a0c73e27d7b3be9ebc9
hamedinzani <ilhaminzani0604@gmail.com>
2021-05-18 01:50:55 +0700

    tampilan login login

--- application/views/user/add.php
+++ application/views/user/add.php
@@ -5,7 +5,7 @@
 
     <div class="row justify-content-center">
 
-        <div class="col-lg-4">
+        <div class="col-lg-5">
 
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body shadow p-0">
@@ -16,48 +16,48 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Create User !</h1>
                                 </div>
-                                <?php echo form_open_multipart('user/add_user');?>
+                                <?php echo form_open_multipart('user/add_user'); ?>
                                 <input type="text" class="form-control form-control-user" id="id" name="id" aria-describedby="" placeholder="" value="<?= $kode_user ?>" hidden>
-                                    <label for="username" class="col-lg-4 col-form-label">Username</label>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="username" name="username" aria-describedby="" placeholder="">
-                                    </div>
-                                    <label for="password" class="col-lg-4 col-form-label">Password</label>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" id="password" name="password" aria-describedby="" placeholder="">
-                                    </div>
-                                    <label for="fullname" class="col-lg-4 col-form-label">Full Name</label>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="fullname" name="fullname" aria-describedby="" placeholder="">
-                                    </div>
-                                    <label for="email_address" class="col-lg-4 col-form-label">Email Address</label>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="email_address" name="email_address" aria-describedby="" placeholder="">
-                                    </div>
-                                    <label for="position" class="col-lg-4 col-form-label">Position</label>
-                                    <div class="form-group">
-                                        <select class="custom-select lg mb-3 col-lg" aria-label=".form-select-lg example" id="position" name="position">
-                                        <?php foreach($position as $p) : ?>
-    <option value="<?php echo $p->id ;?>"> <?php echo $p->position_Name; ?></option>
-   <?php endforeach; ?>
-                                        </select>
-                                    </div>
-                                    <label for="status" class="col-lg-4 col-form-label">Status</label>
-                                    <div class="form-group">
-                                        <select class="custom-select lg mb-3 col-lg" aria-label=".form-select-lg example" id="status" name="status">
-                                             <?php foreach($status as $s) : ?>
-    <option value="<?php echo $s->id ;?>"> <?php echo $s->status_Name; ?></option>
-   <?php endforeach; ?>
-                                             </select>
-                                    </div>
-                                    <label for="gambar" class="col-lg-4 col-form-label">Profile Photo</label>
-                                    <div class="form-group">
-                                        <input type="file" class="form-control form-control-user" id="gambar" name="gambar" aria-describedby="" placeholder="">
-                                    </div>
-                                    <input type="submit" name="submit" class="btn btn-success" value="Simpan"></button>
+                                <label for="username" class="col-lg-4 col-form-label">Username</label>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="username" name="username" aria-describedby="" placeholder="">
+                                </div>
+                                <label for="password" class="col-lg-4 col-form-label">Password</label>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-user" id="password" name="password" aria-describedby="" placeholder="">
+                                </div>
+                                <label for="fullname" class="col-lg-4 col-form-label">Full Name</label>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="fullname" name="fullname" aria-describedby="" placeholder="">
+                                </div>
+                                <label for="email_address" class="col-lg-4 col-form-label">Email Address</label>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="email_address" name="email_address" aria-describedby="" placeholder="">
+                                </div>
+                                <label for="position" class="col-lg-4 col-form-label">Position</label>
+                                <div class="form-group">
+                                    <select class="custom-select lg mb-3 col-lg" aria-label=".form-select-lg example" id="position" name="position">
+                                        <?php foreach ($position as $p) : ?>
+                                            <option value="<?php echo $p->id; ?>"> <?php echo $p->position_Name; ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <label for="status" class="col-lg-4 col-form-label">Status</label>
+                                <div class="form-group">
+                                    <select class="custom-select lg mb-3 col-lg" aria-label=".form-select-lg example" id="status" name="status">
+                                        <?php foreach ($status as $s) : ?>
+                                            <option value="<?php echo $s->id; ?>"> <?php echo $s->status_Name; ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <label for="gambar" class="col-lg-4 col-form-label">Profile Photo</label>
+                                <div class="form-group">
+                                    <input type="file" class="form-control form-control-user" id="gambar" name="gambar" aria-describedby="" placeholder="">
+                                </div>
+                                <input type="submit" name="submit" class="btn btn-success" value="Simpan"></button>
                                 </form>
 
-                                
+
                             </div>
 
                         </div>
