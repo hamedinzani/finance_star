1bc58b3762f5f38639853856925b175edad74c8f
hamedinzani <ilhaminzani0604@gmail.com>
2021-05-25 01:49:29 +0700

    tampilan addrow

--- application/views/quitation/edit.php
+++ application/views/quitation/edit.php
@@ -72,10 +72,13 @@
                     <td><input type="text" id="price" name="price" value=""></td>
                     <td><input type="text" id="cost" name="cost" value=""></td>
                     <td>
-                        <a href="<?php echo base_url('quitation/addrow'); ?>">
-                            <i class="fa fa-plus-circle" style="color:green"></i>
-                            <a href="">
-                                <i class="fa fa-minus-circle" style="color:red"></i>
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+                        </svg> -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+                        </svg>
                     </td>
                 </tr>
                 <tr>
@@ -85,13 +88,13 @@
                     <td><input type="text" id="price" name="price" value=""></td>
                     <td><input type="text" id="cost" name="cost" value=""></td>
                     <td>
-
-                        <a href="<?php echo base_url('quitation/addrow'); ?>">
-                            <i class="fa fa-plus-circle" style="color:green"></i>
-                            <a href="">
-                                <i class="fa fa-minus-circle" style="color:red"></i>
-
-
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+                        </svg>
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+                        </svg> -->
                     </td>
 
                 </tr>
